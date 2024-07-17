01fa17bd108227a0c50918df92c81b4369c5c4f0
marshall235 <fredie1738@gmail.com>
2024-07-17 17:53:57 +0300

    a function that searches for a value in an array of integers using the Linear search algorithm

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,36 +1,36 @@
-#ifndef _SORTING_ALGOS_H
-#define _SORTING_ALGOS_H
+#ifndef _SORTING_ALGOS_H_
+#define _SORTING_ALGOS_H_
 
 #include <stdlib.h>
 #include <stdio.h>
 
 /**
  * struct listint_s - singly linked list
- * 
+ *
  * @n: Integer
  * @index: Index of the node in the list
  * @next: Pointer to the next node
  *
- * Description: singly linked node structure
+ * Description: singly linked list node structure
+ * for Holberton project
  */
 typedef struct listint_s
 {
 	int n;
 	size_t index;
 	struct listint_s *next;
-
 } listint_t;
 
 /**
- * struct skiplist - singly linked list with an express lane
+ * struct skiplist_s - Singly linked list with an express lane
  *
  * @n: Integer
  * @index: Index of the node in the list
  * @next: Pointer to the next node
  * @express: Pointer to the next node in the express lane
  *
- * Description: Singly linked list node structure with an express lane
- * for ALX project
+ * Description: singly linked list node structure with an express lane
+ * for Holberton project
  */
 typedef struct skiplist_s
 {
@@ -40,7 +40,7 @@ typedef struct skiplist_s
 	struct skiplist_s *express;
 } skiplist_t;
 
-int binary_search(int *array, size_t size, int value);
+int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
