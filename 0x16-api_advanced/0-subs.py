ec36b99d7df958a7ab5b898dd7d653a2d201bae9
marshall235 <fredie1738@gmail.com>
2024-09-14 16:30:20 +0300

    a function that queries the Reddit API and returns the number of subscribers (not active users, total subscribers)

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,6 +9,10 @@ import requests
 
 
 def number_of_subscribers(subreddit):
+    """
+    Function that queries the Reddit API
+    - If not a valid subreddit, return 0.
+    """
     req = requests.get(
         "https://www.reddit.com/r/{}/about.json".format(subreddit),
         headers={"User-Agent": "Custom"},
