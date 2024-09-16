f8d8df1c684692652823f93f8619bf569bdc8d84
marshall235 <fredie1738@gmail.com>
2024-09-16 12:58:49 +0300

    Installling flask using puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
-# Using Puppet, install flask from pip3
+# Install Flask 2.1.0 and compatible Werkzeug version
 package { 'flask':
   ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'werkzeug':
+  ensure   => '2.0.3', # compatible version with Flask 2.1.0
   provider => 'pip3',
 }
